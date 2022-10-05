bb142253efcabd30f1c1234558b9c6c36b3ccf68
Daniel L. J <daniellessa.j@gmail.com>
2022-10-05 17:25:56 -0300

    v1

--- messaging-layer/ApacheKafka/config-files/entrypoint-startup.sh
+++ messaging-layer/ApacheKafka/config-files/entrypoint-startup.sh
@@ -4,11 +4,15 @@ echo "#################################"
 echo "export environment variables"
 export JAVA_HOME=/opt/jdk
 export KAFKA_HOME=/opt/apache-kafka
-export PATH=$PATH:$JAVA_HOME:$JAVA_HOME/bin:$KAFKA_HOME:$KAFKA_HOME/bin
+export KAFKA_OPTS="-Djava.security.auth.login.config=/opt/apache-kafka/config/kafka_server_jaas.conf"
+export PATH=$PATH:$JAVA_HOME:$JAVA_HOME/bin:$KAFKA_HOME:$KAFKA_HOME/bin:$KAFKA_OPTS
 
 echo "#################################"
 echo "restart ssh service"
 sudo service ssh restart
+
+echo "############################## create or update admin user in Kafka ##############################"
+/opt/apache-kafka/bin/kafka-configs.sh --zookeeper zookeeper_worker_01:2181,zookeeper_worker_02:2181,zookeeper_worker_03:2181 --alter --add-config 'SCRAM-SHA-512=[password=admin-secret]' --entity-type users --entity-name admin
 
 echo "############################ STOP KAFKA BROKERS ##############################"
 echo "stop Apache Kafka service...BROKER=0"
