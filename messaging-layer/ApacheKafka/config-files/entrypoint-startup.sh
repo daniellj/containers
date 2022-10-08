6bfda7e047e365ad4516c41fec9e3bac4931fd89
Daniel L. J <daniellessa.j@gmail.com>
2022-10-07 21:39:14 -0300

    v1

--- messaging-layer/ApacheKafka/config-files/entrypoint-startup.sh
+++ messaging-layer/ApacheKafka/config-files/entrypoint-startup.sh
@@ -11,11 +11,6 @@ echo "#################################"
 echo "restart ssh service"
 sudo service ssh restart
 
-echo "############################## create or update admin user in Kafka ##############################"
-#/opt/apache-kafka/bin/kafka-configs.sh --zookeeper zookeeper_worker_01:2181,zookeeper_worker_02:2181,zookeeper_worker_03:2181 --alter --add-config 'SCRAM-SHA-512=[password=admin-secret]' --entity-type users --entity-name admin
-/opt/apache-kafka/bin/kafka-configs.sh --bootstrap-server kafka:9092,kafka:9093,kafka:9094 --alter --add-config 'SCRAM-SHA-512=[password=admin-secret]' --entity-type users --entity-name admin --command-config /opt/apache-kafka/config/config.properties
-sleep 3
-
 echo "############################ STOP KAFKA BROKERS ##############################"
 echo "stop Apache Kafka service...BROKER=0"
 /opt/apache-kafka/bin/kafka-server-stop.sh /opt/apache-kafka/config/server-00.properties
@@ -51,6 +46,11 @@ echo "<<<<<<<<############################ STATUS KAFKA BROKER=1 ###############
 service kafka-broker-01 status
 echo "<<<<<<<<############################ STATUS KAFKA BROKER=2 ##############################>>>>>>>>>"
 service kafka-broker-02 status
+
+echo "############################## create or update admin user in Kafka ##############################"
+#/opt/apache-kafka/bin/kafka-configs.sh --zookeeper zookeeper_worker_01:2181,zookeeper_worker_02:2181,zookeeper_worker_03:2181 --alter --add-config 'SCRAM-SHA-512=[password=admin-secret]' --entity-type users --entity-name admin
+/opt/apache-kafka/bin/kafka-configs.sh --bootstrap-server kafka:9092,kafka:9093,kafka:9094 --alter --add-config 'SCRAM-SHA-512=[password=admin-secret]' --entity-type users --entity-name admin --command-config /opt/apache-kafka/config/config.properties
+sleep 3
 
 #echo "<<<<<<<<############################ START KAFKA PRODUCER ##############################>>>>>>>>>"
 #sleep 10
